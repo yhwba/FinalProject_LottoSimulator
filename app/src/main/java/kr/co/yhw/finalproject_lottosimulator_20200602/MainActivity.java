83c051ad041e0999bb00c312968dbe10b03e7477
yhwba <59465345+yhwba@users.noreply.github.com>
2020-06-02 09:45:59 +0900

    프로젝트 기본설정 완료

--- app/src/main/java/kr/co/yhw/finalproject_lottosimulator_20200602/MainActivity.java
+++ app/src/main/java/kr/co/yhw/finalproject_lottosimulator_20200602/MainActivity.java
@@ -1,14 +1,30 @@
 package kr.co.yhw.finalproject_lottosimulator_20200602;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.databinding.DataBindingUtil;
+
+import kr.co.yhw.finalproject_lottosimulator_20200602.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
     }
 }
